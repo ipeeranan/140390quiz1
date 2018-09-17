8035bbe79f36088bcdd4f7ead0c9af57609fe337
unknown <stroms31@gmail.com>
2018-09-17 16:44:28 +0700

    set var msql

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var app = express();
 
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : 'www.db4free.net',
+  user     : 's140390',
+  password : 'abc12345',
+  database : 'db140390'
+});
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The solution is: ', rows[0].solution)
+})
+
+connection.end()
 //app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
