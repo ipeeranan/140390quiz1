6a56e05fc8e762b0abde120acceec9f0478a6c90
unknown <stroms31@gmail.com>
2018-09-17 16:58:11 +0700

    student

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
 });
 
 connection.connect()
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+connection.query('select * from students', function (err, rows, fields) {
     if (err) throw err
   
     console.log('The solution is: ', rows[0].solution)
