a846276b73edf6db8201619b7398e04d50ccdc3c
unknown <stroms31@gmail.com>
2018-09-17 16:48:10 +0700

    edit server

--- server.js
+++ server.js
@@ -5,17 +5,13 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host     : 'www.db4free.net',
   user     : 's140390',
-  password : 'abc12345',
+  password : 'abc123**',
   database : 'db140390'
 });
 
 connection.connect()
 
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
 
-  console.log('The solution is: ', rows[0].solution)
-})
 
 
 //app.use(express.static('static'));
@@ -31,7 +27,9 @@ app.get('/students', function (req, res) {
 app.get('/subject', function (req, res) {
     res.render('pages/subject');
 });
-connection.end()
+
 
 console.log('App is running at http://localhost:8080');
 app.listen(8080);
+
+connection.end()
